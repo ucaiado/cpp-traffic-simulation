5b5c8fb09a0c667d7caa109e10d3b7a54f037eca
Uira Caiado <ucaiado@msn.com>
2021-03-15 00:34:10 -0300

    feat: implement the methods receive and waitForGreen #5

--- traffic-simulation/src/TrafficLight.cpp
+++ traffic-simulation/src/TrafficLight.cpp
@@ -6,18 +6,18 @@
 
 /* Implementation of class "MessageQueue" */
 
-/*
-template <typename T>
-T MessageQueue<T>::receive()
-{
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and
-_condition.wait()
-    // to wait for and receive new messages and pull them from the queue using
-move semantics.
-    // The received object should then be returned by the receive function.
-}
+template <typename T> T MessageQueue<T>::receive() {
+  // FP.5a : The method receive should use std::unique_lock<std::mutex>
+  // and _condition.wait() to wait for and receive new messages and pull them
+  // from the queue using move semantics. The received object should then be
+  // returned by the receive function.
+  std::unique_lock<std::mutex> uLock(_mutex);
+  _cond.wait(uLock, [this] { return !_queue.empty(); });
+  T msg = std::move(_queue.back());
+  _queue.pop_back();
 
-*/
+  return msg;
+}
 
 template <typename T> void MessageQueue<T>::send(T &&msg) {
   // FP.4a : The method send should use the mechanisms
@@ -35,21 +35,23 @@ TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
 }
-
-
-void TrafficLight::waitForGreen()
-{
-    // FP.5b : add the implementation of the method waitForGreen, in which an
-infinite while-loop
-    // runs and repeatedly calls the receive function on the message queue.
-    // Once it receives TrafficLightPhase::green, the method returns.
-}
-
-TrafficLightPhase TrafficLight::getCurrentPhase()
-{
-    return _currentPhase;
-}
 */
+
+void TrafficLight::waitForGreen() {
+  // FP.5b : add the implementation of the method waitForGreen, in which an
+  // infinite while-loop runs and repeatedly calls the receive function on
+  // the message queue. Once it receives TrafficLightPhase::green, the method
+  // returns.
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    auto this_phase = _msgs_queue.receive();
+    if (this_phase == TrafficLightPhase::green) {
+      return;
+    }
+  }
+}
+
+TrafficLightPhase TrafficLight::getCurrentPhase() { return _currentPhase; }
 
 void TrafficLight::simulate() {
   // FP.2b : Finally, the private method „cycleThroughPhases“ should be
